84c152d995a704f555f80f8ed59e14b676008b51
Sonik Sarungale <sonik.sarungale@gmail.com>
2023-02-23 18:06:22 +0530

    Initial

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sonik Sarungale</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i,700,700i|Montserrat:700" rel=stylesheet>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <section id="home">
+        <header>
+            <div class="logo">
+                <div class="text">S</div>
+            </div>
+            <nav class="navigation">
+                <ul>
+                    <li><a href="#">.home()</a></li><li><a href="#">.about()</a></li><li><a href="#">.work()</a></li><li><a href="#">.contact()</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="homecontent">
+            <div class="homeholder">
+                <div class="homecontent-headline">
+                    <h2>👋🏻Hello, I’m Sonik</h2>
+                </div>
+                <div class="homecontent-text">
+                    <h4><span class="txtcolor">Web development wizard</span> who wear a hoodie and have a keyboard for a weapon</h4>
+                    <p>Whether you need a simple landing page, a complex e-commerce platform, a custom web application or a android application, I've got you covered.</p>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    
+    <section id="about" class="sectionstyle">
+        <div class="section-headline">
+            <h2>#about();</h2>
+        </div>
+        <div class="container">
+			<div class="relholder">
+				<div class="content-lhs">
+                    <div class="about">
+                    <p>If you're looking for a web developer who's as cool as a cucumber and always ready to roll with the punches, you've come to the right place.</p>
+                    <p>I've been building websites for 7 years, and I've seen it all. From broken links to funky fonts, nothing fazes me. I've even been known to speak in binary when I'm feeling really geeky.</p>
+                    <p>I'm always up for a good challenge, so if you've got a project that's got you scratching your head, let's team up and make some magic happen!</p>
+                    <p>Here are a few technologies I’ve been working with recently:</p>
+                    <ul class="skills-list">
+                        <li>JavaScript</li>
+                        <li>PHP</li>
+                        <li>React</li>
+                        <li>AWS</li>
+                        <li>Python</li>
+                        <li>Premiere pro</li>
+                        <li>SQL</li>
+                        <li>Node</li>
+                        <li>Adobe XD</li>
+                    </ul>
+                    </div>
+					
+				</div>
+				<div class="img-rhs hide-mob">
+                    <div class="img-box">
+                        <img src="img/potrait.jpg" alt="sonik sarungale">
+                        <div class="imgbgblur"></div>
+                        <div class="imgbgframe"></div>
+                    </div>
+                </div>
+			</div>
+		</div>
+
+    </section>
+    <section id="work" class="sectionstyle">
+        <div class="section-headline">
+            <h2>#work();</h2>
+        </div>
+        <p>Few projects I done while working as a freelancer</p>
+        <div class="container">
+            <div class="work-img-holder">
+				<div class="row">
+					<div class="work-img">
+						<img src="img/work/academicaudit.png" alt="academic audit website" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">Mumbai University Academic Audit portal</h4>
+                                <p>Created portal for MU which was used by more than 800 colleges</p>
+                                <a href="http://academicaudit.muresults.net" class="livedemobtn" target="_BLANK">Live demo</a>
+                            </div>
+					  	</div>
+					</div>
+					<div class="work-img">
+						<img src="img/work/mcc-website.png" alt="mcc website" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">MCC Website</h4>
+                                <p>Mulund college of commerece offical website</p>
+                                <a href="https://mccmulund.ac.in/" class="livedemobtn" target="_BLANK">Live demo</a>
+                                
+                            </div>
+					  	</div>
+					</div>
+					<div class="work-img">
+						<img src="img/work/booksbadlo.png" alt="booksbadlo website" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">Books Badlo</h4>
+                                <p>A startup for exchanging and selling books</p>
+                                <a href="https://booksbadlo.com/" class="livedemobtn" target="_BLANK">Live demo</a>
+                            </div>
+					  	</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="work-img">
+						<img src="img/work/wwa-india.png" alt="wwa india" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">WWA India</h4>
+                                <p>NGO which has been rescuing reptiles and other wild animals for more than a decade</p>
+                                <a href="https://wwaindia.org/" class="livedemobtn" target="_BLANK">Live demo</a>
+                            </div>
+					  	</div>
+					</div>
+					<div class="work-img">
+						<img src="img/work/technobeat.png" alt="technobeat" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">Technobeat</h4>
+                                <p>Annual department fest organised by the I.T & C.S Department
+of Mulund College of Commerce.</p>
+                                <a href="https://mccmulund.ac.in/technobeat/" class="livedemobtn" target="_BLANK">Live demo</a>
+                            </div>
+					  	</div>
+					</div>
+					<div class="work-img">
+						<img src="img/work/spectrum.png" alt="spectrum website" class="image">
+					  	<div class="overlay">
+					  		<div class="text">
+                                <h4 class="projectname">Spectrum</h4>
+                                <p>Annual Intra - Inter Cultural Festival by MCC.</p>
+                                <a href="http://spectrum.mccmulund.ac.in/" class="livedemobtn" target="_BLANK">Live demo</a>
+                            </div>
+					  	</div>
+					</div>
+				</div>
+			</div>
+        
+		</div>
+    </section>
+
+
+</body>
+</html>
